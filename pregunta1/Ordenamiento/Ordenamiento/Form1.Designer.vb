5d5e191022cb1b83c193fcd0c3e9f1de2d204899
titan0000001 <105446302+titan0000001@users.noreply.github.com>
2022-05-17 18:07:22 -0400

    Add files via upload
    Mediante uso de GOTO en visual basic, realice el ordenamiento de 3 números

---
+++ pregunta1/Ordenamiento/Ordenamiento/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.A = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.B = New System.Windows.Forms.TextBox()
+        Me.C = New System.Windows.Forms.TextBox()
+        Me.RES = New System.Windows.Forms.TextBox()
+        Me.ordenar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'A
+        '
+        Me.A.Location = New System.Drawing.Point(12, 12)
+        Me.A.Multiline = True
+        Me.A.Name = "A"
+        Me.A.Size = New System.Drawing.Size(90, 78)
+        Me.A.TabIndex = 0
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'B
+        '
+        Me.B.Location = New System.Drawing.Point(119, 12)
+        Me.B.Multiline = True
+        Me.B.Name = "B"
+        Me.B.Size = New System.Drawing.Size(90, 78)
+        Me.B.TabIndex = 2
+        '
+        'C
+        '
+        Me.C.Location = New System.Drawing.Point(230, 12)
+        Me.C.Multiline = True
+        Me.C.Name = "C"
+        Me.C.Size = New System.Drawing.Size(90, 78)
+        Me.C.TabIndex = 3
+        '
+        'RES
+        '
+        Me.RES.Location = New System.Drawing.Point(12, 226)
+        Me.RES.Multiline = True
+        Me.RES.Name = "RES"
+        Me.RES.Size = New System.Drawing.Size(308, 78)
+        Me.RES.TabIndex = 5
+        '
+        'ordenar
+        '
+        Me.ordenar.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.ordenar.Location = New System.Drawing.Point(12, 135)
+        Me.ordenar.Name = "ordenar"
+        Me.ordenar.Size = New System.Drawing.Size(308, 57)
+        Me.ordenar.TabIndex = 6
+        Me.ordenar.Text = "ORDENAR"
+        Me.ordenar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(335, 317)
+        Me.Controls.Add(Me.ordenar)
+        Me.Controls.Add(Me.RES)
+        Me.Controls.Add(Me.C)
+        Me.Controls.Add(Me.B)
+        Me.Controls.Add(Me.A)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents A As TextBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents B As TextBox
+    Friend WithEvents C As TextBox
+    Friend WithEvents RES As TextBox
+    Friend WithEvents ordenar As Button
+End Class
---
+++ pregunta1/Ordenamiento/Ordenamiento/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    'YURIKA MACUSAYA AGUILAR CI:13846755  PREGUNTA 1
+    Private Sub ordenar_Click(sender As Object, e As EventArgs) Handles ordenar.Click
+        Dim numero1, numero2, numero3 As Integer
+
+        numero1 = Convert.ToInt32(A.Text)
+        numero2 = Convert.ToInt32(B.Text)
+        numero3 = Convert.ToInt32(C.Text)
+
+        Dim li As New List(Of Byte)()
+        li.Add(numero1)
+        If numero2 < li(0) Then li.Insert(0, numero2) : GoTo 1
+        li.Add(numero2)
+1:
+        If numero3 < li(0) Then li.Insert(0, numero3) : GoTo 2
+        If numero3 < li(1) Then li.Insert(1, numero3) : GoTo 2
+        li.Add(numero3)
+
+2:
+        RES.Text = " ASC " & li(0) & ", " & li(1) & ", " & li(2) & "   DESC " & li(2) & ", " & li(1) & ", " & li(0)
+
+    End Sub
+
+
+End Class
